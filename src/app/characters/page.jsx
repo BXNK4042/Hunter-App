5c6ad29a702bf93df74a53eb82b03020a2cf68d1
BXNK4042 <parinthonkongthong@gmail.com>
2025-12-01 21:04:16 +0700

    hunter app v1

--- src/app/characters/page.jsx
+++ src/app/characters/page.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
 import Gon from '../../../public/gon.png'
+import Kilua from '../../../public/Kilua.png'
 
 function page() {
   return (
     <div className='h-[650px]'>
-      <div className="h-full flex justify-center items-center">
-        <div className='w-65 h-120 shadow-2xl flex flex-col bg-black rounded-2xl'>
+      <div className="h-full flex items-center overflow-y-scroll no-scrollbar">
+        <div className='ml-15 mr-5 w-65 h-120 shadow-2xl flex flex-col bg-black rounded-2xl'>
           <div className="flex justify-center items-center">
-            <div className="flex justify-center items-center mt-3 rounded-2xl w-60 h-90 bg-[#BAC3A0] ">
+            <div className="flex justify-center items-center m-3 rounded-2xl w-60 h-90 bg-[#BAC3A0]">
               <Image
                 src={Gon}
                 width={170}
@@ -22,6 +23,24 @@ function page() {
               <p className='ml-2 text-lg'>Freecss</p>            
             </div>
             <p className='text-md'>Nen: Enhancer</p>            
+          </div>
+        </div>        
+        <div className='mr-15 ml-5 w-65 h-120 shadow-2xl flex flex-col bg-black rounded-2xl'>
+          <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center m-3 rounded-2xl w-60 h-90 bg-[#9FB7E4]">
+              <Image
+                src={Kilua}
+                width={170}
+                alt='gon_hunterxhunter'
+              />
+            </div>            
+          </div>
+          <div className="ml-5 text-white">
+            <div className="flex items-end mt-2 ">
+              <h1 className='text-2xl font-bold'>KILUA</h1>
+              <p className='ml-2 text-lg'>Zoldyck</p>            
+            </div>
+            <p className='text-md'>Nen: Transmuter </p>            
           </div>
         </div>        
       </div>
--- src/app/nens/page.jsx
+++ src/app/nens/page.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Image from 'next/image'
+import Nen from '../../../public/Nen.png'
 
 function page() {
   return (
-    <div>
-        
+    <div className='flex flex-col items-center'>
+        <Image
+          src={Nen}
+          width={300}
+          alt='nen-image'
+          className='my-10'
+        />
+        <div className="flex flex-col items-center w-full bg-gray-100 ">
+          <div className="flex flex-col my-10 w-70 gap-5">
+            <div className="">
+              <h1 className='text-lg font-bold'>What is Nen?</h1>
+              <p className='text-xs'>Nen is the fundamental life-energy technique in Hunter × Hunter. It allows a person to control and manipulate their aura, unlocking superhuman strength, speed, perception, and unique abilities.</p>              
+            </div>
+            <hr />
+            <div className="">
+              <h1 className='text-lg font-bold'>Aura</h1>
+              <p className='text-xs'>Aura is the life force that flows through every living being.Nen users can shape and release this energy, turning it into offense, defense, or special abilities.
+              </p>              
+            </div>
+            <hr />
+            <div className="">
+              <h1 className='text-lg font-bold'>The Four Basic Principles</h1>
+              <p className='text-xs'> Ten – Maintains aura around the body for defense. <br />
+                  Zetsu – Shuts off aura to hide presence. <br />
+                  Ren – Increases aura output for power. <br />
+                  Hatsu – The user’s unique ability or personalized attack.
+              </p>              
+            </div>
+            <hr />
+            <div className="">
+              <h1 className='text-lg font-bold'>Nen Types</h1>
+                <p className='text-xs'> Enhancer, Emitter, Manipulator, Transmuter, Conjurer, Specialist <br />
+                    Each user fits into one main category based on their natural affinity.
+                </p>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
